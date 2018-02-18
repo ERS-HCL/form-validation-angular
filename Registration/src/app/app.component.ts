18ee0316c29e351f7dcf3099af38fa191150ba2d
Tejaswani Kunuthuru <36479828+tejareddy123@users.noreply.github.com>
2018-02-18 16:19:50 +0530

    Add files via upload

---
+++ Registration/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  getData(event)
+  {
+    console.log(event);
+  }
+}
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ Registration/src/app/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+  name:string,
+  uid:string,
+  pwd:string,
+  cpwd:string,
+  dob:string,
+  gender:string,
+  country:string,
+  address:string,
+  pincode:string,
+  contact:string,
+  email:string
+}
