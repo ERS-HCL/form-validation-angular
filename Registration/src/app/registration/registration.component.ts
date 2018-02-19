5b296475a19a66aa4a41973c236eb0052789012f
Tejaswani Kunuthuru <36479828+tejareddy123@users.noreply.github.com>
2018-02-19 17:33:43 +0530

    Update registration.component.ts

--- Registration/src/app/registration/registration.component.ts
+++ Registration/src/app/registration/registration.component.ts
@@ -10,11 +10,13 @@ import { FormGroup,FormControl,Validators} from '@angular/forms';
   encapsulation:ViewEncapsulation.Emulated
 })
 export class RegistrationComponent implements OnInit {
-  public myerror;
-  public gender;
 
+  private errorMessage;
+  private gender;
 
+  
   @Output()getobject=new EventEmitter();
+
    countries: string[] = [
     'India',
     'USA',
@@ -25,57 +27,63 @@ export class RegistrationComponent implements OnInit {
     'Italy'
   ];
 
-  selected(e){
-    this.gender=e;
+  selected(genderType:string){
+    this.gender=genderType;
   }
-  validate(ip,cp){
-    if(ip!=cp){
-      this.myerror=" Confirm Password is not matched !";
+
+  validate(password1:string,password2:string){
+    if(password1!=password2){
+      this.errorMessage=" Confirm Password is not matched !";
     }
     else{
-      this.myerror="";
+      this.errorMessage="";
     }
   }
+
   userList:User[]=[];
   form:FormGroup;
+
+  Details;
+  success;
 
   addUser(form){
     this.userList.push(this.form.value);
     console.log(this.userList);
+    this.Details=this.userList;
+    this.success=true;
     }
 
   constructor() { }
 
   ngOnInit() {
-
+ 
     this.form=new FormGroup({
       name:new FormControl("",[Validators.required,
       Validators.pattern("[a-zA-Z][a-zA-Z ]+")]),
       
-      uid:new FormControl("",[Validators.required,
+      userId:new FormControl("",[Validators.required,
       Validators.pattern("[a-zA-Z][a-zA-Z0-9_]+")]),
       
-      pwd:new FormControl("",[Validators.required,
+      password:new FormControl("",[Validators.required,
         ,Validators.minLength(5),
         Validators.maxLength(15)]),
 
-      cpwd:new FormControl("",Validators.required),
+      confirmPassword:new FormControl("",Validators.required),
 
-      dob:new FormControl("",Validators.required),
+      dateOfBirth:new FormControl("",Validators.required),
 
       gender:new FormControl("male",Validators.required),
 
       country:new FormControl("",Validators.required),
 
       address:new FormControl("",[Validators.required,
-      ,Validators.minLength(15),
-      Validators.maxLength(50)]),
+      Validators.minLength(15),Validators.maxLength(50)]),
          
       pincode:new FormControl("",[Validators.required,
-      Validators.pattern('[0-9]*'),Validators.minLength(6),
+      Validators.pattern('[0-9]*'),Validators.maxLength(6),
       Validators.maxLength(6)]),
 
-      contact:new FormControl("",[Validators.required,
+      mobileNumber:new FormControl("",[Validators.required,
       Validators.pattern('^[6|7|8|9][0-9]*'),Validators.minLength(10),
       Validators.maxLength(10)]),
       
@@ -86,7 +94,8 @@ export class RegistrationComponent implements OnInit {
   // To check in console whether data is submitting or not 
 
    ngSubmit=function(userList){
-     this.getobject.emit(userList)
-   }
+   this.getobject.emit(userList);
+     
+      }
 
   }
